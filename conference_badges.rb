428b7a9d88ba590da55acfb7a3fa4e2d0b4cee09
Andrew Waller <andrewhwaller@gmail.com>
2019-03-17 03:51:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(names)
-  names.each do Array.new(new_badges) << { |name| "Hello, my name is #{name}." }
-
+  names.each do Array.new(new_badges) << |name| "Hello, my name is #{name}."
   end
 end
