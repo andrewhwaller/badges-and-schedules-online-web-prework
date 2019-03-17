20a3351f6f9ecca81979548825617c292cc77aaa
Andrew Waller <andrewhwaller@gmail.com>
2019-03-17 04:04:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,5 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(names)
-  names.each_with_index(name, index) { |name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  names.each_with_index { |name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
 end
